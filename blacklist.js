03fe500012547d5131bc05d4a8c3920a0bd15947
ethan-yu0503 <Yu_Family@guest-wireless-upc-1604-10-120-066-066.usc.edu>
2016-10-29 16:12:21 -0700

    Blacklist code
    Implementation of blacklist. remove elements by typing the exact same
    address in the textbox. Not sure if it works LOL

--- blacklist.js
+++ blacklist.js
@@ -4,9 +4,13 @@ var list = new Array(10);
 
 
 function insert(){
-	pets.push("input");
+	list.push("input");
 }
 
 function delete(){
+	var index = list.indexOf("text");
+	if (index > - 1){
+		list.splice(index, 1);
+	}
 
 }
