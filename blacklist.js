b767faa285fdfbcda1c082fb575bad03e3925073
ethan-yu0503 <Yu_Family@guest-wireless-upc-1604-10-120-066-066.usc.edu>
2016-10-29 18:55:59 -0700

    Updated Blacklist

--- blacklist.js
+++ blacklist.js
@@ -1,16 +1,26 @@
-var input = document.createElement("input");
-input.type = "text";
-var list = new Array(10);
-
+//var input = document.createElement("input");
+//input.type = "text";
+// var list = new Array(10);
+alert("Asdf");
 
 function insert(){
-	list.push("input");
+	list.push(document.getElementById('banned_url').value);
+	showUrl();
+	displayDate();
+	alert("insert works");
 }
 
 function delete(){
-	var index = list.indexOf("text");
+	var index = list.indexOf("banned_url");
 	if (index > - 1){
 		list.splice(index, 1);
 	}
+	showUrl();
 
 }
+
+function showUrl(){
+	document.getElementById("showList").innerHTML = list.toString();
+}
+
+
