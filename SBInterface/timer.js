d0ab818304d06f9703efb5508bd530b989e46e0f
Nishir <nishelat@yahoo.com>
2016-10-30 08:22:10 -0700

    Implement Cookies

--- SBInterface/timer.js
+++ SBInterface/timer.js
@@ -13,10 +13,44 @@ var inputtedSTimeMinutes;
 var inputtedCTimeHours;
 var inputtedCTimeMinutes;
 var firebaseRef = firebase.database().ref();
+var temp;
 
+// Testing cookies
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+setCookie("flavor", "vanilla", 10);
 
-var enterButton = document.getElementById("enterButton");
-enterButton.addEventListener('click', function() {
+var extraButton = document.getElementById("extraButton");
+extraButton.addEventListener('click', function() {
 	inputtedSTimeHours = document.getElementById("studyInputHours").value;
 	inputtedSTimeMinutes = document.getElementById("studyInputMins").value;
 	inputtedCTimeHours = document.getElementById("chillInputHours").value;
@@ -27,6 +61,18 @@ enterButton.addEventListener('click', function() {
 	  STimeMinutes: inputtedSTimeMinutes,
 	  CTimeHours : inputtedCTimeHours,
 	  CTimeMinutes : inputtedCTimeMinutes
-	})
+	});
 
 });
+
+function convertTime(Hours, Mins) {
+	return parseInt(Hours) * 36000 + parseInt(Mins) * 60;
+}
+
+document.getElementById('enterButton').addEventListener('click', function() {
+	setCookie("ST", convertTime(inputtedSTimeHours, inputtedSTimeMinutes).toString(), 1);
+	setCookie("CT", convertTime(inputtedCTimeHours, inputtedCTimeMinutes) + "", 1);
+});
+
+/*setCookie("ST", convertTime(inputtedSTimeHours, inputtedSTimeMinutes).toString(), 1);
+setCookie("CT", convertTime(inputtedCTimeHours, inputtedCTimeMinutes) + "", 1);*/
