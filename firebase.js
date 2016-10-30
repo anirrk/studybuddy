a42964fa6bb76954220842a778acb780a54eff81
ethan-yu0503 <Yu_Family@guest-wireless-upc-1604-10-120-066-066.usc.edu>
2016-10-30 03:05:51 -0700

    Still figuring out how to loop through everything

--- firebase.js
+++ firebase.js
@@ -17,29 +17,38 @@ var remove_button = document.getElementById('remove_button');
 var firebaseRef = firebase.database().ref();
 var double = false;
 //const ulList = document.getElementById('list');
+const ulList2 = document.getElementById('listTemp');
 
 var list = new Array();
 var text = document.getElementById("banned_url");
+var childData = new Array();
 showUrl();
+accessData();
+showArray();
 
 
 insert_button.addEventListener('click',function(){
 
-// function insert(){
-	//alert("insert works");
+	
 	var value = blacklist_input.value;
 	for (var i = 0; i < list.length; i++){
 		if (document.getElementById("banned_url").value === list[i]){
 			double = true;
 		}
 	}
-	if (document.getElementById("banned_url").value.indexOf('.') !== -1 &&
+	for (var i = 0; i < childData.length; i++){
+		if (document.getElementById("banned_url").value === childData[i]){
+			double = true;
+		}
+	}
+	
+	if (blacklist_input.value.indexOf('.') !== -1 &&
 		double === false){
 		list.push(document.getElementById("banned_url").value);
 		firebaseRef.child("blacklist").push().set(value);
 
 	}
-	showUrl();
+	showUrlTemp();
 	document.getElementById("banned_url").value = ""; //empty the textbox
 	document.getElementById("banned_url").innerHTML = "";
 
@@ -53,11 +62,12 @@ remove_button.addEventListener('click',function(){
 	if (index != - 1){
 		list.splice(index, 1);
 	}
-	showUrl();
+	showUrlTemp();
 
 });
 
 function showUrl(){
+		//alert("hi");
 
 		const ulList = document.getElementById('blist');
 	
@@ -65,22 +75,42 @@ function showUrl(){
 
 		rootRef.on("child_added", snap => {
 
-		//var website = snap.val();
+			const li = document.createElement('li');
+			li.innerText = snap.val();
+			ulList.appendChild(li); 
 
-		//$("#showList").append(website)
-
-		const li = document.createElement('li');
-		li.innerText = snap.val();
-		ulList.appendChild(li); 
-
-	});
+		});
 	
-	document.getElementById("showList").innerHTML = "";
-	for (var i = 0; i < list.length; i++){
-		document.getElementById("showList").innerHTML += list[i] + "<br>" ;
-		console.log(list[i]);
-	}
 }
+
+/*function showArray(){
+
+	for (var i = 0; i < childData.length; i++){
+		var node = document.createElement("LI");
+    	var textnode = document.createTextNode("Water");
+    	node.appendChild(textnode);
+    	document.getElementById("listTemp").appendChild(node);
+		
+	}
+}*/
+
+function showUrlTemp(){
+	document.getElementById("showList").innerHTML = "";
+		for (var i = 0; i < list.length; i++){
+			document.getElementById("showList").innerHTML += list[i] + "<br>" ;
+			console.log(list[i]);
+		}
+
+}
+
+function accessData(){
+	firebaseRef.once('value', function(snapshot) {
+  		snapshot.forEach(function(childSnapshot) {
+    		//var childKey = childSnapshot.key();
+    		childData = childSnapshot.val();
+  });
+});
+} 
 
 // insert_button.addEventListener('click',function(){
 // 	var value = blacklist_input.value;
